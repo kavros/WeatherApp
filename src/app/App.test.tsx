023a2065547e9a0fbdd4bc920996e35525b6fd6f
Alexandros Kavroulakis <akavroulakis@gmail.com>
2021-11-23 21:40:09 +0000

    adds more UTs

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -6,3 +6,10 @@ test('renders marketing title', () => {
   const title = screen.getByText(/Scotland's most accurate weather service/);
   expect(title).toBeInTheDocument();
 });
+
+test('uses the phrase /the weather is/', () => {
+  render(<App />);
+  const phrase = screen.getAllByText(/, the weather is/)[0];
+
+  expect(phrase).toBeInTheDocument();
+});
